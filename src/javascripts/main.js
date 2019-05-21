0e5eecae11d0309867584de195437b3d0a9cbe14
Saul Solano <saul3d@yahoo.com>
2019-05-20 19:55:26 -0500

    firebase setup

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,14 @@
+import firebase from 'firebase/app';
+import Auth from './components/auth/auth';
+import Birf from './components/birfday/birday';
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  Auth.authStringBuilder();
+  Birf.birfdayStringBuilder();
+};
+
+init();
